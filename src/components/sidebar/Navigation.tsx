9a79f27c2807e35332c665c0972f9977b542f4f7
t-riku <dingariku@icloud.com>
2023-01-19 17:04:22 +0900

    💄 スタイルの調整

--- src/components/sidebar/Navigation.tsx
+++ src/components/sidebar/Navigation.tsx
@@ -10,33 +10,33 @@ import { IoMdSettings } from "react-icons/io";
 const Navigation = () => {
   return (
     <ul className="m-0 p-0">
-      <li className="flex items-center mb-[10px] px-2 py-1 cursor-pointer rounded-xl shadow-md hover:shadow-none hover:translate-y-[4px] transition-all duration-200">
-        <AiFillHome className="text-3xl mr-3" />
-        <span className="text-xl pt-[3px]">ホーム</span>
+      <li className="flex items-center mb-[10px] p-[11px] cursor-pointer rounded-xl shadow-md hover:shadow-none hover:translate-y-[4px] transition-all duration-200">
+        <AiFillHome className="text-2xl mr-3" />
+        <span className="text-xl pt-[2px]">ホーム</span>
       </li>
-      <li className="flex items-center mb-[10px] px-2 py-1 cursor-pointer rounded-xl shadow-md hover:shadow-none hover:translate-y-[4px] transition-all duration-200">
-        <AiOutlineSearch className="text-3xl mr-3" />
-        <span className="text-xl pt-[3px]">検索</span>
+      <li className="flex items-center mb-[10px] p-[11px] cursor-pointer rounded-xl shadow-md hover:shadow-none hover:translate-y-[4px] transition-all duration-200">
+        <AiOutlineSearch className="text-2xl mr-3" />
+        <span className="text-xl pt-[2px]">検索</span>
       </li>
-      <li className="flex items-center mb-[10px] px-2 py-1 cursor-pointer rounded-xl shadow-md hover:shadow-none hover:translate-y-[4px] transition-all duration-200">
-        <IoIosNotifications className="text-3xl mr-3" />
-        <span className="text-xl pt-[3px]">通知</span>
+      <li className="flex items-center mb-[10px] p-[11px] cursor-pointer rounded-xl shadow-md hover:shadow-none hover:translate-y-[4px] transition-all duration-200">
+        <IoIosNotifications className="text-2xl mr-3" />
+        <span className="text-xl pt-[2px]">通知</span>
       </li>
-      <li className="flex items-center mb-[10px] px-2 py-1 cursor-pointer rounded-xl shadow-md hover:shadow-none hover:translate-y-[4px] transition-all duration-200">
-        <BsChatLeftText className="text-3xl mr-3" />
-        <span className="text-xl pt-[3px]">メッセージ</span>
+      <li className="flex items-center mb-[10px] p-[11px] cursor-pointer rounded-xl shadow-md hover:shadow-none hover:translate-y-[4px] transition-all duration-200">
+        <BsChatLeftText className="text-2xl mr-3" />
+        <span className="text-xl pt-[2px]">メッセージ</span>
       </li>
-      <li className="flex items-center mb-[10px] px-2 py-1 cursor-pointer rounded-xl shadow-md hover:shadow-none hover:translate-y-[4px] transition-all duration-200">
-        <BsFillBookmarkCheckFill className="text-3xl mr-3" />
-        <span className="text-xl pt-[3px]">ブックマーク</span>
+      <li className="flex items-center mb-[10px] p-[11px] cursor-pointer rounded-xl shadow-md hover:shadow-none hover:translate-y-[4px] transition-all duration-200">
+        <BsFillBookmarkCheckFill className="text-2xl mr-3" />
+        <span className="text-xl pt-[2px]">ブックマーク</span>
       </li>
-      <li className="flex items-center mb-[10px] px-2 py-1 cursor-pointer rounded-xl shadow-md hover:shadow-none hover:translate-y-[4px] transition-all duration-200">
-        <BsPersonFill className="text-3xl mr-3" />
-        <span className="text-xl pt-[3px]">プロフィール</span>
+      <li className="flex items-center mb-[10px] p-[11px] cursor-pointer rounded-xl shadow-md hover:shadow-none hover:translate-y-[4px] transition-all duration-200">
+        <BsPersonFill className="text-2xl mr-3" />
+        <span className="text-xl pt-[2px]">プロフィール</span>
       </li>
-      <li className="flex items-center mb-[10px] px-2 py-1 cursor-pointer rounded-xl shadow-md hover:shadow-none hover:translate-y-[4px] transition-all duration-200">
-        <IoMdSettings className="text-3xl mr-3" />
-        <span className="text-xl pt-[3px]">設定</span>
+      <li className="flex items-center mb-[10px] p-[11px] cursor-pointer rounded-xl shadow-md hover:shadow-none hover:translate-y-[4px] transition-all duration-200">
+        <IoMdSettings className="text-2xl mr-3" />
+        <span className="text-xl pt-[2px]">設定</span>
       </li>
     </ul>
   );
