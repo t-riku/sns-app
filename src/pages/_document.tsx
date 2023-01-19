e685a247a250d945ac9dda1693c2341490017b01
t-riku <dingariku@icloud.com>
2023-01-19 11:14:45 +0900

    ⚡️ Headにmetaでnameを追加

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="sns-app" content="MyApp" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
