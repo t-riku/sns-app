e9fbc23594b8032a18d41a2a507b4836ea48ec2a
t-riku <dingariku@icloud.com>
2023-01-19 14:49:41 +0900

    ✨ コンポーネントを追加

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,8 +1,18 @@
+import Rightbar from "@/components/rightbar/Rightbar";
+import Sidebar from "@/components/sidebar/Sidebar";
+import Timeline from "@/components/timeline/Timeline";
 import Topbar from "@/components/topbar/Topbar";
 import React from "react";
 
 const Homes = () => {
-  return <Topbar />;
+  return (
+    <>
+      <Topbar />
+      <Sidebar />
+      <Timeline />
+      <Rightbar />
+    </>
+  );
 };
 
 export default Homes;
