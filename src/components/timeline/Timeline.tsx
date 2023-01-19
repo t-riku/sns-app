f3ceafb64279496b08b1b7ca8db6fcd5fc06fc96
t-riku <dingariku@icloud.com>
2023-01-19 17:05:48 +0900

    🎨 シェアとポストコンポーネントを追加

--- src/components/timeline/Timeline.tsx
+++ src/components/timeline/Timeline.tsx
@@ -1,7 +1,16 @@
+import Post from "@/components/timeline/Post";
+import Share from "@/components/timeline/Share";
 import React from "react";
 
 const Timeline = () => {
-  return <div>Timeline</div>;
+  return (
+    <div className="col-span-5 h-screen">
+      <div className="p-5">
+        <Share />
+        <Post />
+      </div>
+    </div>
+  );
 };
 
 export default Timeline;
