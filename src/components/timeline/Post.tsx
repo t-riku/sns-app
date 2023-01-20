ddb08a6ca3cec9e337cb4c4a7102793edba0621e
t-riku <dingariku@icloud.com>
2023-01-20 14:09:55 +0900

    🎨 ダミーデータをmap関数で表示

--- src/components/timeline/Post.tsx
+++ src/components/timeline/Post.tsx
@@ -1,63 +1,63 @@
 import Image from "next/image";
-import me from "public/assets/person/me.jpg";
-import post from "public/assets/post/1.jpeg";
-// import like from "public/assets/heart.png";
+// import me from "public/assets/person/me.jpg";
+// import postImg from "public/assets/post/1.jpeg";
 import React from "react";
 import { AiOutlineMore } from "react-icons/ai";
 import { BsFillHandThumbsUpFill } from "react-icons/bs";
 import { BiComment } from "react-icons/bi";
+import { Users } from "src/Data/dummyData";
 
-const Post = () => {
+const Post = ({ post }: any) => {
+  // const user = Users.filter((users) => users.id === 1);
+
   return (
     <section className="wーfull shadow-lg rounded-xl mx-0 my-7">
-      <div className="p-[10px]">
+      <div className="p-7">
         <div className="flex items-center justify-between">
           <div className="flex items-center">
             <div className="relative aspect-square h-12">
               <Image
                 className="object-cover w-8 h-8 rounded-full cursor-pointer"
-                src={me}
+                src={
+                  Users.filter((users) => users.id === post.id)[0]
+                    .profilePicture
+                }
                 fill
-                placeholder="blur"
+                // placeholder="blur"
                 alt="Picture of the author"
               />
             </div>
-            <span className="text-xl font-semibold mx-[10px]">t-riku</span>
-            <span className="text-xs pt-1 pl-1">5分前</span>
+            <span className="text-xl font-semibold mx-[10px]">
+              {Users.filter((users) => users.id === post.id)[0].username}
+            </span>
+            <span className="text-xs pt-1 pl-1">{post.date}</span>
           </div>
           <div className="">
             <AiOutlineMore />
           </div>
         </div>
         <div className="my-5">
-          <span className="">今日からNextjsの勉強を始めました!</span>
+          <span className="p-3 my-4">{post.desc}</span>
           <div className="relative aspect-square mt-5 max-h-[400px] w-full">
             <Image
               className="object-contain w-8 h-8 cursor-pointer"
-              src={post}
+              src={post.photo}
               fill
-              placeholder="blur"
+              // placeholder="blur"
               alt="Picture of the post"
             />
           </div>
         </div>
         <div className="flex items-center justify-between mb-2">
           <div className="flex items-center">
-            {/* <div className="relative aspect-square h-6 mr-2">
-              <Image
-                className="object-cover w-8 h-8 cursor-pointer"
-                src={like}
-                fill
-                placeholder="blur"
-                alt="like icon"
-              />
-            </div> */}
             <BsFillHandThumbsUpFill className="mr-1 cursor-pointer text-blue-600" />
-            <span className="text-[15px]">5人がいいねを押しました</span>
+            <span className="text-[15px]">
+              {post.like}人がいいねを押しました
+            </span>
           </div>
           <div className="flex items-center cursor-pointer border-b border-gray-400 mr-1 text-sm">
             <BiComment className="mr-1" />
-            <span className="pb-1">4</span>
+            <span className="pb-1">{post.comment}</span>
           </div>
         </div>
       </div>
--- src/components/timeline/Timeline.tsx
+++ src/components/timeline/Timeline.tsx
@@ -1,15 +1,17 @@
 import Post from "@/components/timeline/Post";
 import Share from "@/components/timeline/Share";
 import React from "react";
+import { Posts } from "src/Data/dummyData";
 
 const Timeline = () => {
+  console.log(Posts);
   return (
     <section className="col-span-6 h-screen">
       <div className="p-5">
         <Share />
-        <Post />
-        <Post />
-        <Post />
+        {Posts.map((post) => {
+          return <Post post={post} key={post.id} />;
+        })}
       </div>
     </section>
   );
