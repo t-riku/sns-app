21685d44dcb85370c6b435216aa2c5910a3d1998
t-riku <dingariku@icloud.com>
2023-01-20 21:19:26 +0900

    🎨 Homeコンポーネントに変更

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,8 +12,8 @@ const Home: NextPage = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {/* <Homes /> */}
-      <Profile />
+      <Homes />
+      {/* <Profile /> */}
     </div>
   );
 };
