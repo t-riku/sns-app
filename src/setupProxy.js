9cae44cc4a4ff77eb94f599ca777860d2944fb59
t-riku <dingariku@icloud.com>
2023-01-25 15:44:52 +0900

    🚑 proxyの設定・corsエラーの解消

---
+++ src/setupProxy.js
@@ -0,0 +1,12 @@
+// const { createProxyMiddleware } = require("http-proxy-middleware");
+
+// module.exports = function (app) {
+//   app.use(
+//     "/api",
+//     createProxyMiddleware({
+//       // target: "http://10.0.2.2:4000",
+//       target: "http://localhost:4000",
+//       changeOrigin: true,
+//     })
+//   );
+// };
