3df4498260a4359d75fd39d48e09d5a85c6c664b
t-riku <dingariku@icloud.com>
2023-01-26 15:13:16 +0900

    🎨 Linkのパスの変更

--- src/components/topbar/Title.tsx
+++ src/components/topbar/Title.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const Title = () => {
   return (
     <div className="col-span-3 font-mono">
-      <Link href="/home/Home">
+      <Link href="/Home">
         <span className="text-2xl text-white ml-5 bold">SNS App</span>
       </Link>
     </div>
